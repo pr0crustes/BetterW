de7609a68959f8c0f7fa53e351aa0d8b3e20f2f6
pr0crustes <dark.pr0crustes@gmail.com>
2018-09-13 19:09:02 -0300

    Fixing space 2

--- src/tweak/_Pr0_Utils.mm
+++ src/tweak/_Pr0_Utils.mm
@@ -5,6 +5,7 @@ NSString* FUNCTION_prefGet(NSString *key) {
     return [[NSDictionary dictionaryWithContentsOfFile:MACRO_PLIST] valueForKey:key];
 }
 
+
 bool FUNCTION_prefGetBool(NSString *key) {
     return [FUNCTION_prefGet(key) boolValue];
 }
