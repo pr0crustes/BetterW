3b9f1d00f3172603cc1b7de5588e3c38c5890a17
pr0crustes <dark.pr0crustes@gmail.com>
2018-09-18 18:12:31 -0300

    Removed unecessary stuff

--- src/tweak/headers/WAChatStorage.h
+++ src/tweak/headers/WAChatStorage.h
@@ -1,5 +1,9 @@
 #import "headers/WAMessage.h"
 
 @interface WAChatStorage : NSObject
-	-(void)revokeIncomingMessage:(WAMessage *)message updatedStanzaID:(id)arg2 outOfOrder:(_Bool)arg3 revokeDate:(NSString *)date;
+@end
+
+
+@interface WAChatStorage (Revoke)
+	-(void)revokeIncomingMessage:(WAMessage *)message updatedStanzaID:(id)arg2 outOfOrder:(_Bool)arg3 revokeDate:(id)arg4 deferAction:(id)arg5;
 @end
