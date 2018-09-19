e6a21eb54a545fdc79ca2ce703cd13ffb541173c
pr0crustes <dark.pr0crustes@gmail.com>
2018-09-19 10:40:35 -0300

    Removed unused stuff.

--- src/tweak/headers/WAProfilePictureDynamicThumbnailView.h
+++ src/tweak/headers/WAProfilePictureDynamicThumbnailView.h
@@ -1,5 +1,2 @@
 @interface WAProfilePictureDynamicThumbnailView : UIImageView
-    -(void)layoutSubviews;
-    -(void)setProfilePictures:(id)arg1;
-    -(void)updateProfilePictureCacheItemWithJID:(id)arg1;
 @end
