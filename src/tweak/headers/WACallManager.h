d33378686fe0ea29f1f1243da46f45a75c7f7077
pr0crustes <dark.pr0crustes@gmail.com>
2018-09-24 17:40:29 -0300

    d

--- src/tweak/headers/WACallManager.h
+++ src/tweak/headers/WACallManager.h
@@ -1,4 +1,4 @@
 @interface WACallManager : NSObject
     // Starts a call (voice or video).
-    -(void)internalAttemptOutgoingVoiceCallWithJID:(NSString*)contactJID callUISource:(int)arg2 withVideo:(_Bool)arg3;
+    -(void)internalAttemptOutgoingVoiceCallWithJID:(NSString*)contactJID callUISource:(int)arg2 withVideo:(_Bool)isVideo;
 @end
