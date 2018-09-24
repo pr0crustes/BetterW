b193a7684fb444a1a0ceba5feea006c01dae9cb6
pr0crustes <dark.pr0crustes@gmail.com>
2018-09-24 17:32:19 -0300

    Removed unused macro

--- src/tweak/_Pr0_Utils.h
+++ src/tweak/_Pr0_Utils.h
@@ -12,18 +12,6 @@
 #define MACRO_log_enabling(message) NSLog(@"[BetterW] -> Enabling:  -%@-", message)
 
 
-// A simple macro to bring back what was once UIAlert, helps to reduce code repetition
-#define MACRO_present_alert_with(presentView, handlerYes, handlerNo) \
-{ \
-    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Confirmation" message:@"Are you sure you want to call?" preferredStyle:UIAlertControllerStyleAlert]; \
-    UIAlertAction* yesAction = [UIAlertAction actionWithTitle:@"Yes" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) { handlerYes }]; \
-    UIAlertAction* noAction = [UIAlertAction actionWithTitle:@"No" style:UIAlertActionStyleDefault handler:^(UIAlertAction * action) { handlerNo }]; \
-    [alert addAction:yesAction]; \
-    [alert addAction:noAction]; \
-    [presentView presentViewController:alert animated:YES completion:nil]; \
-}
-
-
 // A simple macro to check if a contactJID is from a group
 #define MACRO_is_contactJID_group(contactJID) ([contactJID rangeOfString:@"-"].location != NSNotFound)
 
