c1517c413d25ce3728791e12dbf9d7e8665f34f8
pr0crustes <dark.pr0crustes@gmail.com>
2018-10-09 16:48:06 -0300

    Commented methods.

--- src/tweak/headers/WAStatusViewerViewController.h
+++ src/tweak/headers/WAStatusViewerViewController.h
@@ -2,9 +2,12 @@
 #import "WAActionSheetPresenter.h"
 
 @interface WAStatusViewerViewController : UIViewController
+	// Handler called to go to the next Status Item, after timer.
     -(void)statusViewerSectionViewDidFinishPresentingItem:(id)arg1;
+	// Called to add all actions buttons to the WAActionSheetPresenter.
     -(void)addButtonsToActionSheet:(WAActionSheetPresenter *)actionSheet forIncomingStatusMessage:(id)arg2;
-	-(WAStatusItem*)currentStatusItem;
+	// WAStatusItem getter.
+	-(WAStatusItem *)currentStatusItem;
 
 	// New
 	-(void)pr0crustes_saveStatus;
