c9820aadbf9845dc74720d5097f3c4f4ffa1d33f
pr0crustes <dark.pr0crustes@gmail.com>
2019-08-22 20:08:16 -0300

    Added bitcoin address.

--- src/betterw_prefs/betterw_prefs_RootListController.m
+++ src/betterw_prefs/betterw_prefs_RootListController.m
@@ -11,6 +11,11 @@
 		return _specifiers;
 	}
 
+	-(void)onClickBitcoinAddress:(id)arg1 {
+		UIPasteboard* pasteboard = [UIPasteboard generalPasteboard];
+		pasteboard.string = @"1CvcFZAMpzWzhPQzQ6vZdejSwTNuwWrPy5";
+	}
+
 	// Just a button that opens the project on github.
 	-(void)onClickSourceCode:(id)arg1 {
 		[[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://github.com/pr0crustes/BetterW"] options:@{} completionHandler:nil];
